649a4b816b5b7583a09d1154aecdf8395b6c9449
rcasachi <rcasachi@gmail.com>
2025-12-23 12:53:40 -0300

    teste

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  background: 'hsl(var(--background))',
+  foreground: 'hsl(var(--foreground))',
+  primary: 'hsl(var(--primary))',
+  'primary-foreground': 'hsl(var(--primary-foreground))',
+  border: 'hsl(var(--border))',
+  input: 'hsl(var(--input))',
+  ring: 'hsl(var(--ring))',
+}
+
+export const radii = {
+  lg: 'var(--radius)',
+  md: 'calc(var(--radius) - 2px)',
+  sm: 'calc(var(--radius) - 4px)',
+}
